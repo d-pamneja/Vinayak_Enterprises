fee17af0e907a1d447828098173c24c653d57019
d-pamneja <dpamneja@gmail.com>
2024-08-10 14:38:18 +0530

    properties break

--- header.php
+++ header.php
@@ -1,29 +1,29 @@
 <!-- ***** Header Area Start ***** -->
 <header class="header-area header-sticky">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <nav class="main-nav">
-                        <!-- ***** Logo Start ***** -->
-                        <a href="index.php" class="logo">
-                            <h1><?php include 'title.php'?></h1>
-                        </a>
-                        <!-- ***** Logo End ***** -->
-                        <!-- ***** Menu Start ***** -->
-                        <ul class="nav">
-                            <li><a href="index.php" class="active">Home</a></li>
-                            <li><a href="properties.php">Properties</a></li>
-                            <li><a href="property-details.php">Property Details</a></li>
-                            <li><a href="contact.php">Contact Us</a></li>
-                            <li><a href="#"><i class="fa fa-calendar"></i> Schedule a visit</a></li>
-                        </ul>
-                        <a class='menu-trigger'>
-                            <span>Menu</span>
-                        </a>
-                        <!-- ***** Menu End ***** -->
-                    </nav>
-                </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
+                    <!-- ***** Logo Start ***** -->
+                    <a href="index.php" class="logo">
+                        <h1><?php include 'title.php'?></h1>
+                    </a>
+                    <!-- ***** Logo End ***** -->
+                    <!-- ***** Menu Start ***** -->
+                    <ul class="nav">
+                        <li><a href="index.php" class="active">Home</a></li>
+                        <li><a href="properties.php">Properties</a></li>
+                        <li><a href="property-details.php">Property Details</a></li>
+                        <li><a href="contact.php">Contact Us</a></li>
+                        <li><a href="#"><i class="fa fa-calendar"></i> Schedule a visit</a></li>
+                    </ul>
+                    <a class='menu-trigger'>
+                        <span>Menu</span>
+                    </a>
+                    <!-- ***** Menu End ***** -->
+                </nav>
             </div>
         </div>
-    </header>
-    <!-- ***** Header Area End ***** -->
+    </div>
+</header>
+<!-- ***** Header Area End ***** -->
